b2f5e616e2debb9edf36724b3b033fea627cac30
Matthias Dittberner <matthias.dittberner@web.de>
2019-07-09 17:50:00 +0200

    Add split file feature in File class

--- v1/thread_safe/file.hpp
+++ v1/thread_safe/file.hpp
@@ -1,12 +1,16 @@
 #ifndef FILE_HPP
 #define FILE_HPP
 
-#include <algorithm>
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include <math.h>
 #include <mutex>
 #include <string>
+#include <thread>
+#include <vector>
+
+#include "../timer.hpp"
 
 class File {
     public:
@@ -38,7 +42,7 @@ class File {
                 double tmp = mSize;
                 for(i=0; i<=4; i++) {
                     if(tmp >= 1024) {
-                        tmp = mSize/std::pow(2,(i+1)*10);
+                        tmp = mSize/pow(2,(i+1)*10);
                     } else {
                         break;
                     }
@@ -73,6 +77,40 @@ class File {
             return(mUnit);
         }
 
+        void split(unsigned int num_chunks) {
+            Timer t;
+            t.start();
+            unsigned int orig_pos = mFile.tellg();
+            mFile.seekg(0, std::ios::end);
+            unsigned int end = mFile.tellg();
+            mFile.seekg(orig_pos, std::ios::beg);
+            std::vector<unsigned int> indices(num_chunks);
+            unsigned int offset = end/num_chunks;
+            unsigned int from = 0;
+            unsigned int to   = offset;
+            std::vector<std::thread> mThreads;
+            // Use a thread pool instead e.g. what if num_chunks > num of hardware threads?
+            for(int i=0; i<num_chunks; i++) {
+                mThreads.emplace_back([=](){
+                    write_chunk(from, to, i+1);
+                });
+                
+                from = to;
+                to   = to + offset;
+                if(to > end) {
+                    to = end;
+                }
+            }
+            for(auto &t: mThreads) {
+                if(t.joinable()) {
+                    t.join();
+                }
+            }
+            t.stop();
+            double throughput = double(size()) / t.elapsed() * 1000;
+            std::cout << "Through put: " << throughput << " " << mUnit << "/s" << std::endl;
+        }
+
 
     private:
         std::string mFilename;
@@ -81,6 +119,36 @@ class File {
         double mSize = -1;
         std::string mUnit = "bytes";
 
+        void fast_copy(unsigned int from, uint to, uint chunk_id) {
+            std::ifstream file("myfile", std::ios::binary | std::ios::ate);
+            std::streamsize size = file.tellg();
+            file.seekg(0, std::ios::beg);
+
+            std::vector<char> buffer(size);
+            if (file.read(buffer.data(), size)) {
+
+            }
+        }
+
+        void write_chunk(unsigned int from, uint to, uint chunk_id) {
+            std::unique_lock<std::mutex> ulock(mutex_file_operation);
+            ulock.unlock();
+            std::string filename = mFilename + "." + std::to_string(chunk_id);
+            File fin(mFilename);
+            std::ofstream fout(filename);
+            std::cout << "Processing chunk " << chunk_id << " ..." << std::endl;
+            // uint orig = mFile.tellg();
+            char ch;
+            fin.mFile.seekg(from, std::ios::beg);
+            for(int i=from; i<to; i++) {
+                // ulock.lock();
+                // mFile.seekg(i,std::ios::beg);
+                ch = char(fin.mFile.get());
+                // ulock.unlock();
+                fout.put(ch);
+            }
+            fout.close();
+        }
 
 };
 
